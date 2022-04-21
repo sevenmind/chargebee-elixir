a3624967719d5cc501bdd3416a3bcbfb04e4ae9e
Grant McLendon <grant@7mind.de>
2022-04-21 13:20:44 +0200

    :recycle: merge customers only accepts two arguments, expose them directly

--- lib/chargebee_elixir/customer.ex
+++ lib/chargebee_elixir/customer.ex
@@ -4,7 +4,15 @@ defmodule ChargebeeElixir.Customer do
   """
   use ChargebeeElixir.Resource, "customer"
 
-  def merge(args) do
-    create(args, "/merge")
+
+  @doc """
+  Merge `from_customer_id` into `to_customer_id`
+
+  This API moves a customer's payment methods, subscriptions, invoices, credit
+  notes, transactions, unbilled charges, and orders to another customer. Events
+  and email logs will not be moved. The API execution is asynchronous.
+  """
+  def merge(to_customer_id, from_customer_id) do
+    create([to_customer_id: to_customer_id, from_customer_id: from_customer_id], "/merge")
   end
 end

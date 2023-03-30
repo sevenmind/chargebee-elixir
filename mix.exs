a4c824611a24d3f52b8ef43ae97f2d856dda1e8e
Grant McLendon <grant@7mind.de>
2023-03-30 13:47:29 -0500

    🔖 v0.2.4 - Add support for NonSubscriptions.one_time_purchase/2

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule ExChargebee.MixProject do
       },
       source_url: "https://github.com/sevenmind/ex_chargebee",
       homepage_url: "https://github.com/sevenmind/ex_chargebee",
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
